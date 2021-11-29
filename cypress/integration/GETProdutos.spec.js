1d343e2381bb16de6fa24d137e9a817911c4ad65
Wil Merchant <wtmerchant3@gmail.com>
2021-11-28 19:48:14 -0500

    First cypress project commit

---
+++ cypress/integration/GETProdutos.spec.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+describe('Products api', () => {
+    context('GET /produtos', () => {
+        it('should return a list with all products', () => {
+            cy.request({
+                method: 'GET',
+                url: 'https://serverest.dev/produtos'
+            })
+                .should((response) => {
+                    // cy.log(JSON.stringify(response.body))
+                    expect(response.status).to.eq(200)
+                    expect(response.body.quantidade).to.eq(45)
+                    expect(response.body.produtos.length).to.be.eq(45);
+                    expect(response.body.produtos[0]).to.have.all.keys(
+                        'nome', 'preco', 'descricao', 'quantidade', '_id'
+                      )
+                });
+        });
+    });
+});
